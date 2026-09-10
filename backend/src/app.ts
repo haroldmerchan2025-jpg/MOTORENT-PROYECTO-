9b05d9ab70ae7e91409e5c09dbab17e76e841b78
Harold <haroldmerchan2025@gmail.com>
2026-09-09 23:16:19 -0500

    integracion de prisma a backend y borrar dependencias de mysql y sequelize

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor MotoRent con TypeScript funcionando');
+});
+
+export default app;
