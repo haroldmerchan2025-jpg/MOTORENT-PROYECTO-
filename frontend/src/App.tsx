03797ae32b692554d25f1e23b5ca4cec603bc4e8
Harold <haroldmerchan2025@gmail.com>
2026-09-20 19:34:45 -0500

    creacion paginas frotnend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+
 import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Dashboard from "./pages/Dashboard/dashboard";
 import Motos from "./pages/Motos/Motos";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Perfil from "./pages/perfil/perfil";
 import Rentas from "./pages/Rentas/rentas";
+import PublicarMoto from "./pages/Publicarmoto/publicarmoto";
 
 function App() {
   return (
     <BrowserRouter>
+
+     <Navbar />
+
+     
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route element={<ProtectedRoute />}>
-        <Route path="/motos" element={<Motos />} />
-      </Route>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/rentas" element={<Rentas />} />
+          <Route path="/publicarmoto" element={<PublicarMoto />} />
+        </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/motos" element={<Motos />} />
       </Routes>
     </BrowserRouter>
   );
